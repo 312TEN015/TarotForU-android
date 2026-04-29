5d6cb6d72a734bc639b35b7261001d5094b9c44c
dlawldn10 <dlawldn10@gmail.com>
2026-04-29 16:52:07 +0900

    Feat: Add demo version and refactor architecture - update version

--- Tarot/app/build.gradle.kts
+++ Tarot/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.fourleafclover.tarot"
         minSdk = 24
         targetSdk = 35
-        versionCode = 18
-        versionName = "2.0"
+        versionCode = 19
+        versionName = "3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
